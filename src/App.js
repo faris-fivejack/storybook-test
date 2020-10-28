eaf396afcf8a08d4a9136d6419afb603e2be71c5
faris-fivejack <faris.ramadhan@fivejack.com>
2020-10-28 17:00:44 +0700

    usage sample on App.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {
-  ContainedButton
+  ContainedButton,
+  OutlineButton
 } from './components/atoms'
 
 function App() {
   return (
     <div className="App" style={{display: 'flex', flexDirection: 'row', margin: 0, padding: 0, width: '100vw', height: '100vh', justifyContent: 'center', alignItems: 'center'}}>
-      <ContainedButton
-        backgroundColor={null}
-        color={null}
-        differentiator="Purchasing"
-        disabled={false}
-        height_type="Big"
-        label="Click me"
-        onClick={() => {}}
-        width_type="Full"
-      />
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+        <ContainedButton
+          backgroundColor={null}
+          color={null}
+          differentiator="Purchasing"
+          disabled={false}
+          height_type="Big"
+          label="Click me"
+          onClick={() => {}}
+          width_type="Full"
+        />
+        <OutlineButton
+          backgroundColor={null}
+          color={null}
+          differentiator="Regular"
+          disabled={false}
+          height_type="Big"
+          label="Title"
+          onClick={() => {}}
+          width_type="Full"
+        />
+      </div>
     </div>
   );
 }
